8649b8cab94170ffa30b35ccd167e84849d4e2cb
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-04-10 18:22:42 +0300

    feat: updated menu example

--- MenuExample/MenuExample.cs
+++ MenuExample/MenuExample.cs
@@ -4,85 +4,92 @@ using CounterStrikeSharp.API.Core.Capabilities;
 using CounterStrikeSharp.API.Modules.Commands;
 using T3MenuSharedApi;
 
-namespace MenuTest;
+namespace MenuExample;
 
-public class MenuTest : BasePlugin
+public class MenuExample : BasePlugin
 {
     public override string ModuleAuthor => "T3Marius";
-    public override string ModuleName => "MenuExample";
+    public override string ModuleName => "T3Menu-Example";
     public override string ModuleVersion => "1.0";
+    public int PlayerVotes;
 
     public IT3MenuManager? MenuManager;
-
-    // get the instance
-    public IT3MenuManager? GetMenuManager()
+    public IT3MenuManager? GetMenuManager() // this function is used to get the menu manager
     {
         if (MenuManager == null)
+        {
             MenuManager = new PluginCapability<IT3MenuManager>("t3menu:manager").Get();
-
+        }
         return MenuManager;
     }
-
     public override void Load(bool hotReload)
     {
-
+        
     }
-    [ConsoleCommand("css_menu")]
-    public void onmenu(CCSPlayerController player, CommandInfo info)
+    [ConsoleCommand("css_menutest")]
+    public void OnTest(CCSPlayerController? player, CommandInfo info)
     {
-        // get the manager and check of nullabilty
-        var manager = GetMenuManager();
+        if (player == null)
+            return;
+
+        var manager = GetMenuManager(); // get the manager using the function we've created.
+
         if (manager == null)
             return;
 
-        // create menu
-        var mainMenu = manager.CreateMenu("Menu Test", isSubMenu: false); // you can add freezePlayer, hasSound too but you can disable them from config directly
+        IT3Menu menu = manager.CreateMenu($"Example Menu | Votes: {PlayerVotes}", isSubMenu: false); // if this isn't a sub menu you don't even need to call this.
 
-        // normal option
-        mainMenu.Add("Normal Option", (p, option) =>
+        menu.AddOption("Normal Option", (p, o) =>
         {
-            player.PrintToChat("Normal option selected");
-            manager.CloseMenu(player); // you can close the menu on select if you want (optional) 
+            p.PrintToChat("This is a normal option!");
         });
 
-        // bool option
-        mainMenu.AddBoolOption("Bool Option", defaultValue: true, (p, option) =>
+        menu.AddOption("Vote Option", (p, o) =>
         {
-            if (option is IT3Option boolOption)
+            p.PrintToChat("You added 1 vote!");
+            PlayerVotes++;
+            menu.Title = $"Example Menu | Votes: {PlayerVotes}"; // call the title again and then refresh
+            manager.Refresh(); // you can also add a repeat if you use manager.Refresh(1) when press it will refresh every second.
+        });
+        menu.AddBoolOption("Bool Option", defaultValue: true, (p, o) =>
+        {
+            if (o is IT3Option boolOption)
             {
-                bool isEnabled = boolOption.OptionDisplay!.Contains("✔"); // adding this will automaticly show the ✔ and X based on the value.
-                player.PrintToChat(isEnabled ? "Enabled" : "Disabled");
+                bool isEnabled = boolOption.OptionDisplay!.Contains("✔"); // this is how you check if the option is enabled or not.
+
+                if (isEnabled)
+                {
+                    p.PrintToChat("Bool Option is enabled!");
+                }
+                else
+                {
+                    p.PrintToChat("Bool Option is disabled!");
+                }
             }
         });
 
-        List<object> numbers = new List<object> { 10, 20, 30 };
-        mainMenu.AddSliderOption(display: "", customValues: numbers, defaultValue: 0, onSlide: (p, option) =>
-        {
-            int value = (int)option.SliderValue!;
-            player.PrintToChat($"Value: {value}");
-        });
-        List<object> text = new List<object> { "test", "another test", "hello" };
-        mainMenu.AddSliderOption(display: "", customValues: text, defaultValue: 0, onSlide: (p, option) =>
-        {
-            string text = (string)option.SliderValue!;
-            player.PrintToChat($"Value: {text}");
-        });
+        // prepare a list of objects for the slider option
+        List<object> intList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        List<object> stringList = ["day", "week", "month", "year"];
 
-        // text option
-        mainMenu.AddTextOption("<font color='#FFFF00'>THIS IS A TEXT OPTION</font>"); // you can set color like that for example now is yellow
-
-        // creating sub menu
-        mainMenu.Add("Sub Menu", (p, option) => // you need to add it as an option to mainMenu first
+        menu.AddSliderOption("Int List", values: intList, defaultValue: 1, displayItems: 3, (player, option, index) =>
         {
-            var subMenu = manager.CreateMenu("Sub Menu", isSubMenu: true);
-            subMenu.ParentMenu = mainMenu; // set the mainMenu as parent menu for subMenu to propely navigate trough them with back button
-
-            subMenu.Add("This is a sub option", (p, option) =>
+            if (option is IT3Option sliderOption && sliderOption.DefaultValue != null)
             {
-                player.PrintToChat("This is a sub option");
-            });
-            manager.OpenSubMenu(player, subMenu); // opening the submenu
-        }); ;
-        manager.OpenMainMenu(player, mainMenu); // opening mainmenu
+                int selectedValue = (int)sliderOption.DefaultValue; // convert the default value to int, this is what player pressed in the slider.
+                player.PrintToChat($"Selected int: {selectedValue}");
+            }
+        });
+        menu.AddSliderOption("String List", values: stringList, defaultValue: stringList[0], displayItems: 3, (player, option, index) =>
+        {
+            if (option is IT3Option sliderOption && sliderOption.DefaultValue != null)
+            {
+                string selectedValue = (string)sliderOption.DefaultValue; // convert the default value to string, this is what player pressed in the slider.
+                player.PrintToChat($"Selected string: {selectedValue}");
+            }
+        });
+
+        manager.OpenMainMenu(player, menu); // open the menu using the manager.
+
     }
 }
