5d30a1b300c9fa70e49e2360b6509df5b88dbec8
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-05-03 03:45:27 +0300

    feat: addde InputOption

--- T3Menu-API/src/T3MenuAPI/Classes/T3Menu.cs
+++ T3Menu-API/src/T3MenuAPI/Classes/T3Menu.cs
@@ -1,24 +1,30 @@
 ﻿using CounterStrikeSharp.API.Core;
-using Microsoft.Extensions.Logging;
+using CounterStrikeSharp.API.Modules.Utils;
 using T3MenuAPI;
+using static T3MenuAPI.T3MenuAPI;
 
 public class T3Menu : IT3Menu
 {
-    public string? Title { get; set; } = "";
+    public string Title { get; set; } = string.Empty;
+    public int MaxTitleLenght { get; set; } = Instance.Config.Settings.MaxTitleLenght;
+    public int MaxOptionLenght { get; set; } = Instance.Config.Settings.MaxOptionLenght;
     public LinkedList<IT3Option> Options { get; set; } = new();
     public LinkedList<IT3Option>? Prev { get; set; } = null;
     public bool FreezePlayer { get; set; } = true;
     public bool HasSound { get; set; } = true;
     public IT3Menu? ParentMenu { get; set; } = null;
     public bool IsSubMenu { get; set; } = false;
+    public bool IsExitable { get; set; } = true;
     public bool showDeveloper { get; set; } = true;
     public Action<CCSPlayerController, IT3Option, int>? OnSlide { get; set; }
     public int LastSelectedIndex { get; set; } = 0;
     public LinkedListNode<IT3Option> AddOption(string display, Action<CCSPlayerController, IT3Option> onChoice, bool isDisabled = false)
     {
+        string color = Instance.Config.Settings.DisabledOptionColor;
+
         T3Option newOption = new()
         {
-            OptionDisplay = isDisabled ? $"<font color='#34282C'>{display}</font>" : display,
+            OptionDisplay = isDisabled ? $"<font color='{color}'>{display}</font>" : display,
             OnChoose = isDisabled ? null! : onChoice,
             Index = Options.Count,
             Parent = this,
@@ -97,14 +103,48 @@ public class T3Menu : IT3Menu
 
         return Options.AddLast(newBoolOption);
     }
+    public LinkedListNode<IT3Option> AddInputOption(string display, string placeHolderText = "", Action<CCSPlayerController, IT3Option, string>? onInputSubmit = null, string? inputPromptMessage = null)
+    {
+        if (Options == null)
+            Options= new();
+
+        T3Option newInputOption = new()
+        {
+            OptionDisplay = $"{display}: [<font color='grey'>{placeHolderText}</font>]",
+            Index = Options.Count,
+            Parent = this,
+            Type = OptionType.Input,
+            DefaultValue = placeHolderText,
+            OnChoose = (player, option) =>
+            {
+                if (option is T3Option inputOption)
+                {
+                    var menuPlayer = Players.Values.FirstOrDefault(p => p.player == player);
+                    if (menuPlayer != null)
+                    {
+                        menuPlayer.InputMode = true;
+                        menuPlayer.CurrentInputOption = inputOption;
+                    }
+                    if (!string.IsNullOrEmpty(inputPromptMessage))
+                    {
+                        player.PrintToChat(inputPromptMessage);
+                    }
+                }
+            }
+        };
+        newInputOption.OnInputSubmit = onInputSubmit;
+        return Options.AddLast(newInputOption);
+    }
     public void AddTextOption(string display, bool selectable = false)
     {
         if (Options == null)
             Options = new();
 
+        string color = Instance.Config.Settings.TextOptionColor;
+
         T3Option newTextOption = new()
         {
-            OptionDisplay = display,
+            OptionDisplay = $"<font color='{color}'>{display}</font>",
             Parent = this,
             Type = OptionType.Text,
             IsDisabled = !selectable
